8f93e9708c0f09ad27cf47573858553b27d3d17c
EustasTheMonk <kononenko.dima2001@gmail.com>
2024-12-18 20:18:58 +0200

    first commit

---
+++ layouts/default.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+<!--  <v-app-bar scroll-behavior="hide" scroll-threshold="320">-->
+<!--    <v-app-bar-title>-->
+<!--      <p>v-app-bar-title</p>-->
+<!--    </v-app-bar-title>-->
+<!--  </v-app-bar>-->
+  <p>dsadasdasdsd</p>
+  <slot/>
+</template>
+
+<style scoped>
+
+</style>
